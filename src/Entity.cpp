39f2f46fa9143f37d4be2c30cee0b4b9b6b7e7d7
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-03-28 23:08:34 -0400

    add basic entity class

---
+++ src/Entity.cpp
@@ -0,0 +1,97 @@
+#include "Entity.h"
+
+int roundAwayFromZero(float x) { return x < 0 ? floor(x) : ceil(x); }
+
+Entity::Entity() {
+  int cx = 0;
+  int cy = 0;
+
+  spawn_x = cx;
+  spawn_y = cy;
+
+  // TODO:
+  // vx = MAX_SPEED;
+  // vy = MAX_AIR_SPEED;
+
+  shape = sf::RectangleShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  shape.setFillColor(sf::Color::Red);
+  shape.setPosition(cx, cy);
+
+  isDying = false;
+}
+
+Entity::Entity(int cx, int cy, float init_vx = 0, float init_vy = 0) {
+  spawn_x = cx;
+  spawn_y = cy;
+
+  // vx = MAX_SPEED;
+  // vy = MAX_AIR_SPEED;
+
+  shape = sf::RectangleShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  shape.setFillColor(sf::Color::Red);
+  shape.setPosition(cx, cy);
+
+  isDying = false;
+}
+
+Entity::~Entity() {}
+
+sf::RectangleShape Entity::getShape() {
+  return shape;
+}
+
+void Entity::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+  target.draw(shape, states);
+}
+
+bool Entity::isGrounded(GameState& state) {
+  sf::Vector2<float> pos = shape.getPosition();
+  sf::Vector2<float> size = shape.getSize();
+
+  for (int i = 0; i < size.x; i++) {
+    if (state.checkCollision(pos.x + i, pos.y + size.y + 1)) {
+      // snap to ground
+      float newY = pos.y + size.y;
+      while (state.checkCollision(pos.x + i, newY)) {
+        newY -= 1;
+      }
+
+      shape.setPosition(pos.x, newY - CELL_SIZE);
+
+      vy = 0;
+      return true;
+    }
+  }
+
+  vy = MAX_AIR_SPEED;
+  return false;
+}
+
+void Entity::update(GameState& state) {}
+
+void Entity::MoveEntity(float xoffset, float yoffset, GameState& state) {
+  shape.move(xoffset, yoffset);
+}
+
+void Entity::die() {
+  if (!isDying) {
+    // on first invocation, change color
+    // TODO: shape.setFillColor(sf::Color::Yellow); // replace with change to dead texture
+    isDying = true;
+  }
+
+  // disable horizontal movement
+  vx = 0;
+}
+
+void Entity::reset() {
+  shape = sf::RectangleShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  shape.setFillColor(sf::Color::Red);
+  shape.setPosition(spawn_x, spawn_y);
+
+  // vx = MAX_SPEED;
+  // vy = MAX_AIR_SPEED;
+
+  isDying = false;
+}
+
---
+++ src/Entity.h
@@ -0,0 +1,100 @@
+#include <SFML/Graphics.hpp>
+
+#include "GameState.h"
+
+#define MAX_SPEED 400.0f
+#define DECEL_RATE 3000.0f
+#define ACCEL_RATE 3000.0f
+#define JUMP_FORCE 1150.0f
+#define AIR_DECEL_RATE (DECEL_RATE * 1.0f)
+#define MAX_AIR_SPEED (MAX_SPEED * 5.0f)
+
+class Entity : public sf::Drawable {
+public:
+  /**
+   * @brief Construct a new Entity object
+   *
+   * @param cx The spawnpoint x
+   * @param cy The spawnpoint y
+   */
+  Entity(int cx, int cy);
+
+  /**
+   * @brief Destroy the Entity object
+   */
+  ~Entity();
+
+  /**
+   * @brief Update loop for the entity called every frame.
+   *
+   * @param state The current game state.
+   */
+  void update(GameState& state);
+
+  /**
+   * @brief Get the underlying sf::Shape object for the entity.
+   *
+   * @return The underlying sf::Shape object.
+   */
+  sf::RectangleShape getShape();
+
+  /**
+   * @brief Kill the entity.
+   */
+  void die();
+
+  /**
+   * @brief Reset the entity to its spawnpoint.
+   */
+  void reset();
+
+private:
+
+  /**
+   * @brief The entity's spawnpoint.
+   */
+  int spawn_x, spawn_y;
+
+  /**
+   * @brief The entity's velocity.
+   */
+  float vx, vy;
+
+  /**
+   * @brief The underlying shape of the entity.
+   */
+  sf::RectangleShape shape;
+
+  /**
+   * @brief Flag for whether the entity is dying.
+   */
+  bool isDying;
+
+
+  /**
+   * @brief API for drawing the entity from a window.
+   *
+   * @example window.draw(entity);
+   * @param target The window to draw the entity on.
+   * @param states The render states to apply to the entity.
+   */
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+  /**
+   * @brief Check if the entity is grounded.
+   *
+   * @param state The current game state.
+   * @return Whether the entity is grounded.
+   */
+  bool isGrounded(GameState& state);
+
+  /**
+   * @brief Move the entity.
+   *
+   * @param xoffset The x offset.
+   * @param yoffset The y offset.
+   * @param state The current game state.
+   */
+  void MoveEntity(float xoffset, float yoffset, GameState& state);
+};
+
