c5d264227623bf664c213f5986533c06c2899186
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-03-30 18:06:01 -0400

    remove all GameState code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-// #include "GameState.h"
 #include "Game.h"
 
 using namespace std;
 
 int main() {
-    // GameState game;
-    // game.runGame();
-
     Game::getInstance()->run();
     return 0;
 }
